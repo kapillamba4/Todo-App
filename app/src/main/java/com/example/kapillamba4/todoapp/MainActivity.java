b567ab2bffb3d06ce51f75ebfe5a6f514cf8cbe1
Kapil <kapillamba4@gmail.com>
2017-09-26 21:07:15 +0530

    Initial commit

---
+++ app/src/main/java/com/example/kapillamba4/todoapp/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.kapillamba4.todoapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        this.setTitle("TodoList");
+
+    }
+}
